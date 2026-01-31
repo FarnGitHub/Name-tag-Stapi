68e3af84317fa294d231710302315a272c1b4645
Farn <93525542+FarnGitHub@users.noreply.github.com>
2026-01-31 13:31:24 +0700

    finalize

--- src/main/java/farn/nametag/mixin/EntityTrackerEntryMixin.java
+++ src/main/java/farn/nametag/mixin/EntityTrackerEntryMixin.java
@@ -1,8 +1,5 @@
 package farn.nametag.mixin;
 
-import com.llamalad7.mixinextras.expression.Definition;
-import com.llamalad7.mixinextras.expression.Expression;
-import farn.nametag.NameTagMain;
 import farn.nametag.other.EntityNameTag;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.entity.EntityTrackerEntry;
@@ -15,20 +12,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.List;
 
 @Mixin(EntityTrackerEntry.class)
-public class OmgWhyMixin {
+public class EntityTrackerEntryMixin {
 
     @Shadow
     public Entity currentTrackedEntity;
 
     @Shadow
-    public int ticks;
-
-    @Shadow
-    public int trackingFrequency;
+    public boolean newPlayerDataUpdated;
 
     @Inject(method="notifyNewLocation", at = @At("TAIL"))
     public void onSendAround(List par1, CallbackInfo ci) {
-        if(this.ticks % this.trackingFrequency == 0 && currentTrackedEntity instanceof EntityNameTag tagged && tagged.nametag_entityHasNameTag()) {
+        if(this.newPlayerDataUpdated && currentTrackedEntity instanceof EntityNameTag tagged && tagged.nametag_entityHasNameTag()) {
             tagged.nametag_updateClientNameTag();
         }
     }
